dad4e645445b48d08c9a58b93a44e5712d5282ec
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-08-01 14:23:17 +0300

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 
 import "./App.css";
+import test from "/test.jpg";
 
 function App() {
-  return <div class="bg-red-500">hvybjn</div>;
+  return (
+    <div
+      style={{ backgroundImage: `url(${test})` }}
+      className=" w-[100%] h-[100vh] bg-cover bg-no-repeat bg-center"
+    >
+      <div class="bg-black/50 h-[100vh] flex flex-col justify-center items-center">
+        <p className="text-white text-2xl md:text-5xl">
+          We are building something cool for colorndrive
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;

58302e607e8c783406586414e381f66e9c979f5a
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-08-05 09:51:03 +0300

    Finished steps mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 
 import "./App.css";
 import Steps from "./components/Steps";
-
+import WhyUs from "./components/WhyUs";
 function App() {
   return (
     <div>
       <Steps />
+      <WhyUs />
     </div>
   );
 }

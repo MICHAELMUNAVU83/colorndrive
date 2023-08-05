84615aa44a7739ce8cc866708d830380f3bc4a2d
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-08-05 12:07:55 +0300

    Finished steps mobile

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import WhyUs from "./components/WhyUs";
 import Reviews from "./components/Reviews";
 import Footer from "./components/Footer";
 import AboutUs from "./components/AboutUs";
+import Services from "./components/Services";
 function App() {
   return (
     <div>
       <AboutUs />
+      <Services />
       <Steps />
       <WhyUs />
       <Reviews />
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React from "react";
+import servicesbg from "/servicesbg.png";
 
 const Services = () => {
+  const services = [
+    {
+      id: 1,
+      image: "/services1.png",
+      title: "Gerneral Car services",
+      content:
+        "Our skilled team provides meticulous    cleaning and restoration for both the  exterior and interior of your car, along    with specialized treatments   ",
+    },
+    {
+      id: 2,
+      image: "/services3.png",
+      title: "Interior& Exterior wash",
+      content:
+        "Our skilled team provides meticulous    cleaning and restoration for both the  exterior and interior of your car, along    with specialized treatments   ",
+    },
+    {
+      id: 3,
+      image: "/services3.png",
+      title: "Wrap",
+      content:
+        "Our skilled team provides meticulous    cleaning and restoration for both the  exterior and interior of your car, along    with specialized treatments   ",
+    },
+    {
+      id: 4,
+      image: "/services4.png",
+      title: "Tint",
+      content:
+        "Our skilled team provides meticulous    cleaning and restoration for both the  exterior and interior of your car, along    with specialized treatments   ",
+    },
+    {
+      id: 5,
+      image: "/services5.png",
+      title: "Steam Wash",
+      content:
+        "Our skilled team provides meticulous    cleaning and restoration for both the  exterior and interior of your car, along    with specialized treatments   ",
+    },
+    {
+      id: 6,
+      image: "/services6.png",
+      title: "Alignment",
+      content:
+        "Our skilled team provides meticulous    cleaning and restoration for both the  exterior and interior of your car, along    with specialized treatments   ",
+    },
+  ];
   return (
-    <div>Services</div>
-  )
-}
+    <div
+      className="md:h-[1582px]  "
+      style={{ backgroundImage: `url(${servicesbg})` }}
+    >
+      <div className="flex flex-col w-[100%] justify-center gap-12 p-12 items-center">
+        <div className="flex flex-col w-[100%] justify-center items-center">
+          <p className="text-xl md:text-2xl">Our Services</p>
+          <p className="bg-[#FB0000] md:w-[30%] w-[80%] h-[3px]"></p>
+        </div>
 
-export default Services
+        <div className="grid grid-cols-3 gap-16">
+          {services.map((service) => (
+            <div className="flex flex-col bg-white cursor-pointer hover:scale-105 transition-all ease-in-out duration-500 rounded-md h-[500px] w-[100%]   ">
+              <img
+                src={service.image}
+                alt=""
+                className="h-[273px] rounded-t-md w-[100%] object-cover"
+              />
+              <div className="flex flex-col p-4 h-[100%] justify-between">
+                <p className="text-xl font-bold">{service.title}</p>
+                <p className="text-start w-[90%] text-[#4B4453] mx-auto">
+                  {service.content}
+                </p>
+                <p className="text-end font-semibold text-[#F50606] p-2">Find out more</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;

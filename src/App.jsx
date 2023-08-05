d41196bcaefef429bd33499a20fe59f5fa8de8a3
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-08-05 10:12:21 +0300

    Finished steps mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import "./App.css";
 import Steps from "./components/Steps";
 import WhyUs from "./components/WhyUs";
+import Reviews from "./components/Reviews";
 function App() {
   return (
     <div>
       <Steps />
       <WhyUs />
+      <Reviews />
     </div>
   );
 }
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import reviewsbg from "/reviewsbg.png";
+
+const Reviews = () => {
+  return (
+    <div
+      className="bg-cover w-[100%] bg-center min-h-[100vh]"
+      style={{
+        backgroundImage: `url(${reviewsbg})`,
+      }}
+    >
+      <div className="bg-white/30 h-[100%] w-[100%]  flex flex-col items-center">
+        <div className="flex flex-col w-[100%] justify-center items-center">
+          <p className="text-xl md:text-2xl">Customers Says</p>
+          <p className="bg-red-500 md:w-[30%] w-[80%] h-[3px]"></p>
+        </div>
+
+        <div className="flex md:flex-row flex-col justify-between p-4 w-[100%]">
+          <div className="bg-[#F3F4F6]/50  flex h-[400px]  justify-between flex-col md:w-[30%]">
+            <div className="flex justify-start p-2 items-start">
+              <img
+                src="/blackquotes.png"
+                alt=""
+                className="h-[24px] w-[24px] object-contain"
+              />
+            </div>
+            <div className="w-[90%] text-center mx-auto">
+              Car & Drive Auto Detailing exceeded all my expectations with their
+              outstanding service! From the moment I entered, their friendly
+              staff made me feel welcome. Their attention to detail was
+              remarkable as they diligently cleaned every corner of my car,
+              leaving it looking and smelling brand new. The skilled technicians
+              used top-of-the-line products, resulting in a gleaming exterior
+              and revitalized interior.
+            </div>
+            <div className="gap-2 flex items-center">
+              <p className="w-[150px] bg-red-500 h-[2px]" />
+
+              <p className="P-4">Michael Munavu</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;

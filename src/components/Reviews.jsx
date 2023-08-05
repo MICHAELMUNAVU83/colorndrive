c53b673289b3958324e5c0f1c1cabd30d3ffc1ee
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-08-05 10:14:07 +0300

    Finished steps mobile

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -16,7 +16,55 @@ const Reviews = () => {
         </div>
 
         <div className="flex md:flex-row flex-col justify-between p-4 w-[100%]">
-          <div className="bg-[#F3F4F6]/50  flex h-[400px]  justify-between flex-col md:w-[30%]">
+          <div className="bg-[#F3F4F6]/50 p-2 flex h-[400px]  justify-between flex-col md:w-[30%]">
+            <div className="flex justify-start p-2 items-start">
+              <img
+                src="/blackquotes.png"
+                alt=""
+                className="h-[24px] w-[24px] object-contain"
+              />
+            </div>
+            <div className="w-[90%] text-center mx-auto">
+              Car & Drive Auto Detailing exceeded all my expectations with their
+              outstanding service! From the moment I entered, their friendly
+              staff made me feel welcome. Their attention to detail was
+              remarkable as they diligently cleaned every corner of my car,
+              leaving it looking and smelling brand new. The skilled technicians
+              used top-of-the-line products, resulting in a gleaming exterior
+              and revitalized interior.
+            </div>
+            <div className="gap-2 flex items-center">
+              <p className="w-[150px] bg-red-500 h-[2px]" />
+
+              <p className="P-4">Michael Munavu</p>
+            </div>
+          </div>
+
+          <div className="bg-black p-2  flex h-[400px]  justify-between flex-col md:w-[30%]">
+            <div className="flex justify-start p-2 items-start">
+              <img
+                src="/whitequotes.png"
+                alt=""
+                className="h-[24px] w-[24px] object-contain"
+              />
+            </div>
+            <div className="w-[90%] text-white text-center mx-auto">
+              Car & Drive Auto Detailing exceeded all my expectations with their
+              outstanding service! From the moment I entered, their friendly
+              staff made me feel welcome. Their attention to detail was
+              remarkable as they diligently cleaned every corner of my car,
+              leaving it looking and smelling brand new. The skilled technicians
+              used top-of-the-line products, resulting in a gleaming exterior
+              and revitalized interior.
+            </div>
+            <div className="gap-2 flex items-center">
+              <p className="w-[150px] bg-red-500 h-[2px]" />
+
+              <p className="text-white">Michael Munavu</p>
+            </div>
+          </div>
+
+          <div className="bg-[#F3F4F6]/50 p-2 flex h-[400px]  justify-between flex-col md:w-[30%]">
             <div className="flex justify-start p-2 items-start">
               <img
                 src="/blackquotes.png"

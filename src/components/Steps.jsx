02ae9303a34605334fef96a781fe485579d901a5
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-08-05 09:34:08 +0300

    Finished steps mobile

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -11,25 +11,25 @@ const Steps = () => {
     >
       <div className="bg-white/30 h-[100%] w-[100%]  flex flex-col items-center">
         <div className="flex flex-col w-[100%] justify-center items-center">
-          <p>Easy Quick Following Steps</p>
-          <p className="bg-red-500 w-[30%] h-[3px]"></p>
+          <p className="text-xl md:text-2xl">Easy Quick Following Steps</p>
+          <p className="bg-red-500 md:w-[30%] w-[80%] h-[3px]"></p>
         </div>
-        <div className="flex justify-between p-4 w-[100%">
-          <div className="flex flex-col w-[30%] items-center">
+        <div className="flex md:flex-row flex-col justify-between p-4 w-[100%">
+          <div className="flex flex-col md:w-[30%] items-center">
             <img
               src="/step1.png"
               alt=""
               className="h-[526px] w-[100%] object-contain"
             />
           </div>
-          <div className="flex flex-col w-[30%] items-center">
+          <div className="flex flex-col md:w-[30%] items-center">
             <img
               src="/step2.png"
               alt=""
               className="h-[526px] w-[100%] object-contain"
             />
           </div>
-          <div className="flex flex-col w-[30%] items-center">
+          <div className="flex flex-col md:w-[30%] items-center">
             <img
               src="/step3.png"
               alt=""
